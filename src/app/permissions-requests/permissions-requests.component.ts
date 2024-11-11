e439f320295c2cec6047302d82e3845c98718f96
TymoteuszB <Tymoteusz.91100@gmail.com>
2024-11-11 10:28:53 +0100

    positioned content component

--- src/app/permissions-requests/permissions-requests.component.ts
+++ src/app/permissions-requests/permissions-requests.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { TopSubMenuComponent } from "../top-sub-menu/top-sub-menu.component";
 
 @Component({
   selector: 'app-permissions-requests',
   standalone: true,
-  imports: [TopSubMenuComponent],
+  imports: [],
   templateUrl: './permissions-requests.component.html',
   styleUrl: './permissions-requests.component.scss'
 })
