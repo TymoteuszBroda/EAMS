2095f1a34873c9d9d7be1f49f9818c99f2f9f624
TymoteuszB <Tymoteusz.91100@gmail.com>
2024-11-11 10:41:43 +0100

    added permissions component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { TopMenuComponent } from "./top-menu/top-menu.component";
 import { TopSubMenuComponent } from './top-sub-menu/top-sub-menu.component';
 import { ContentListComponent } from "./content-list/content-list.component";
+import { PermissionsComponent } from "./permissions/permissions.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopMenuComponent, TopSubMenuComponent, ContentListComponent],
+  imports: [RouterOutlet, TopMenuComponent, TopSubMenuComponent, ContentListComponent, PermissionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
