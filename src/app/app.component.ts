da090149b1136bb754d63887ff245ba390696fd1
TymoteuszB <Tymoteusz.91100@gmail.com>
2024-11-11 09:54:48 +0100

    changed position of top and topSub menus to fixed, added content coponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenuComponent } from "./top-menu/top-menu.component";
 import { TopSubMenuComponent } from './top-sub-menu/top-sub-menu.component';
+import { ContentListComponent } from "./content-list/content-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TopMenuComponent, TopSubMenuComponent],
+  imports: [RouterOutlet, TopMenuComponent, TopSubMenuComponent, ContentListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
